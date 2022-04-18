ebed96699431de04fa9021a6841f6a0671d254c9
Russell White <356303+russwyte@users.noreply.github.com>
2022-04-18 14:09:48 -0500

    more tests

--- src/test/scala/fis/ch3/ListSpec.scala
+++ src/test/scala/fis/ch3/ListSpec.scala
@@ -2,7 +2,11 @@ package fis.ch3
 import List._
 
 class ListSpec extends munit.FunSuite:
-  test("cons `::` method works") {
+  test("cons `::`") {
     val l = 1 :: Nil
     assert(l.equals(List.Cons(1, Nil)))
+  }
+  test("folLeft") {
+    val l = 1 :: 2 :: Nil
+    assert(l.foldLeft(0) { (b, a) => b + a } == 3)
   }
